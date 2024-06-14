bffa7a69eac853083b4ef30f1f4d0729ed78e26e
hyvip-ai <rm2932002@gmail.com>
2024-06-15 03:24:34 +0530

    Design update

--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignUp } from '@clerk/nextjs';
 import React from 'react';
 
 export default function Signup() {
   return (
     <div className='h-full w-full justify-center items-center'>
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
